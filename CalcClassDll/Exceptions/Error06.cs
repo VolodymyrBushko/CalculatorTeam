7315c03aee75d4fc07f4d6ea7e78d0a27e583db8
Andriy <aolijnyk73@gmail.com>
2020-05-12 17:50:13 +0300

    Add Exception for Calc

---
+++ CalcClassDll/Exceptions/Error06.cs
@@ -0,0 +1,9 @@
+﻿using System; 
+
+namespace CalcClassDll.Exceptions
+{
+    public class Error06: Exception
+    {
+        public override string Message => "Error06 : Very small or very large value of the number";
+    }
+}
